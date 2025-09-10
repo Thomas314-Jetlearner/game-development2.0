a4f44a76971fab3f09328902bdb00c856148e517
Thomas <thomast6556@gmail.com>
2025-09-10 20:04:45 +0100

    alien game

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,1 +1,24 @@
-print("wdfgjkkjhgfdxs")
+import pgzrun
+from pgzero.builtins import *
+import pgzero.screen
+screen: pgzero.screen.Screen
+
+
+
+WIDTH = 700
+HEIGHT = 700
+
+Maat = Actor("alein2.0")
+
+def draw():
+    screen.blit("space", (0,0))
+    Maat.draw()
+
+def on_mouse_down(pos):
+    if Maat.collidepoint(pos):
+        print("AWESOME")
+
+
+
+
+pgzrun.go()
