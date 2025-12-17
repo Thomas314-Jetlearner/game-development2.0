69f84dc9a3bf34e799f8fb7e0796c7c2f66ad10d
Thomas <thomast6556@gmail.com>
2025-12-17 19:09:11 +0000

    x

--- main1.py
+++ main1.py
@@ -7,13 +7,14 @@ HEIGHT =900
 
 questions = []
 
-with open("project 4\questions copy.txt", "r" ) as file:
+with open("project 4/questions copy.txt", "r" ) as file:
     data = file.readlines()
 
     for l in data:
         ques = l.split("|")
-        gingerbread = {"question":[0], "option": [1:5], "answer":[5] }
-        questions.append(gingerbread)
+        print(ques)
+        ginger = {"questions":[0], "option":[1:5], "answer":[5]}
+        questions.append(ginger)
 
 powder = random.choice(questions)
 
